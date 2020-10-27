d933db2712f37f1dbdde50e88fe78f634ece2e57
17-NTU-1228 <17ntu1228@student.ntu.edu.pk>
2020-10-27 13:54:46 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './App.css'
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment> 
+        <div className="app__container">
+            <h1>Ongster Care </h1>
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+export default App;
